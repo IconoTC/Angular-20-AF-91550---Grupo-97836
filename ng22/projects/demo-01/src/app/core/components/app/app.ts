42a01404797d654b4bbbacc280684dee50ab6778
Alejandro Cerezo <alce65@hotmail.es>
2026-06-16 17:47:21 +0200

    Add sample component and test it

--- ng22/projects/demo-01/src/app/core/components/app/app.ts
+++ ng22/projects/demo-01/src/app/core/components/app/app.ts
@@ -1,16 +1,45 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sample } from '../sample/sample';
 
 @Component({
   selector: 'ind-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sample],
   template: `
-    <h1 class="title">Hola, {{ title() }}</h1>
-    <router-outlet />
+    <main class="container">
+      <router-outlet />
+      <ind-sample />
+    </main>
   `,
 
-  styles: ``,
+  styles: `
+    :host {
+      display: grid;
+      grid-template-rows: auto 1fr auto;
+      min-height: 100vh;
+      font-family: var(--font-family);
+      margin: 0;
+      padding: 0;
+    }
+    main.container {
+      padding: 1rem 2rem;
+      width: 100%;
+      min-height: 90%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 1rem;
+      position: relative;
+    }
+    @media (width > 600px) {
+      .wide {
+        align-self: stretch;
+        margin-inline: 5rem;
+      }
+    }
+  `,
 })
 export class App {
-  protected readonly title = signal('Demo 01');
+  // protected readonly title = signal('Demo 01');
 }
