812421f487c88e51e775f860ffb3f13fc2d9c6fc
Alejandro Cerezo <alce65@hotmail.es>
2026-06-17 11:31:24 +0200

    Add Angular demo made components in About Page

--- ng22/projects/demo-01/src/app/features/about/about-page.ts
+++ ng22/projects/demo-01/src/app/features/about/about-page.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { LogoNg } from './components/logo-angular/logo-ng';
+import { SeparatorRwd } from './components/separator-rwd/separator-rwd';
+import { Pills } from './components/pills/pills';
 
 @Component({
   selector: 'ind-about-page',
-  imports: [],
-  template: ` <h2>About</h2> `,
+  imports: [LogoNg, SeparatorRwd, Pills],
+  template: `
+    <div>
+      <ind-logo-ng />
+      <h2>{{ pageTitle() }}</h2>
+    </div>
+    <ind-separator-rwd />
+    <ind-pills />
+  `,
   styleUrls: ['../pages.css'],
-  styles: ``,
+  styles: `
+    :host {
+      display: flex;
+      flex-direction: column;
+    }
+    @media (width > 800px) {
+      :host {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 3rem;
+      }
+    }
+  `,
 })
-export default class AboutPage {}
+export default class AboutPage {
+  protected readonly pageTitle = signal('About ng22');
+}
